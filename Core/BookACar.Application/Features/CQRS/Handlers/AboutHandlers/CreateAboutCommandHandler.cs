7c2aea6a7ee83f6996ff637b713835fdc6824ba4
Betulsnyz <snyzbetul@gmail.com>
2026-02-19 12:57:57 +0300

    About sınıfı için CQRS yazıldı

---
+++ Core/BookACar.Application/Features/CQRS/Handlers/AboutHandlers/CreateAboutCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using BookACar.Application.Features.CQRS.Commands.AboutCommands;
+using BookACar.Application.Interfaces;
+using BookACar.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookACar.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class CreateAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public CreateAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateAboutCommand command)
+        {
+            await _repository.CreateAsync(new About
+            {
+                Title = command.Title,
+                Description = command.Description,
+                ImageUrl = command.ImageUrl
+            });
+        }
+    }
+}
