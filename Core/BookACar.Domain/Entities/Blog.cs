9ae34b3120ea869f5fc4f0d44bdbeff79019f129
Betulsnyz <snyzbetul@gmail.com>
2026-03-03 15:45:34 +0300

    migration yapıldı blog ve author eklendi

---
+++ Core/BookACar.Domain/Entities/Blog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookACar.Domain.Entities
+{
+    public class Blog
+    {
+        public int BlogID { get; set; }
+        public string Title { get; set; }
+        public string AuthorID { get; set; }
+        public Author Author { get; set; }
+        public List<Blog> Blogs { get; set; }
+        public string CoverImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
