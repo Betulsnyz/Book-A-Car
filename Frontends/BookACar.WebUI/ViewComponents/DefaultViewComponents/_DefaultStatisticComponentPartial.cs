b8f01a6419c182a85279be6833cb38463d2bb0f9
Betulsnyz <snyzbetul@gmail.com>
2026-03-05 15:54:44 +0300

    bağlantılar düzeltiliyor

---
+++ Frontends/BookACar.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookACar.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultStatisticComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
