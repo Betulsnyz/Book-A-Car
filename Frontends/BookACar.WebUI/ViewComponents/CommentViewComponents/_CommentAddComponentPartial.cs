9f702436f0a21e28a7767349ac234cf4f8aa4dae
Betulsnyz <snyzbetul@gmail.com>
2026-03-11 14:28:57 +0300

    yorum componenti tamamlandı

---
+++ Frontends/BookACar.WebUI/ViewComponents/CommentViewComponents/_CommentAddComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookACar.WebUI.ViewComponents.CommentViewComponents
+{
+    public class _CommentAddComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
