c5705a2fc055dfab2ab95125a0ce6dc202442cff
Betulsnyz <snyzbetul@gmail.com>
2026-04-06 14:24:50 +0300

    comment entity ve generic repository eklentisi

---
+++ Core/BookACar.Domain/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookACar.Domain.Entities
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Description { get; set; }
+        public Blog Blog { get; set; }
+        public int BlogID { get; set; }
+    }
+}
