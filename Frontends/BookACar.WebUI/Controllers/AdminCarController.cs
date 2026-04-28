6389c77e308f8ceeefc2d2c4a7140650d32ebed5
Betulsnyz <snyzbetul@gmail.com>
2026-04-28 09:48:01 +0300

    create car tamamlandı

--- Frontends/BookACar.WebUI/Controllers/AdminCarController.cs
+++ Frontends/BookACar.WebUI/Controllers/AdminCarController.cs
@@ -1,6 +1,9 @@
-﻿using BookACar.Dto.CarDtos;
+﻿using BookACar.Dto.BrandDtos;
+using BookACar.Dto.CarDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace BookACar.WebUI.Controllers
 {
@@ -17,7 +20,7 @@ namespace BookACar.WebUI.Controllers
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7273/api/Cars/GetCarWithBrand");
-            if(responseMessage.IsSuccessStatusCode)
+            if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDtos>>(jsonData);
@@ -27,8 +30,37 @@ namespace BookACar.WebUI.Controllers
         }
 
         [HttpGet]
-        public IActionResult CreateCar()
+        public async Task<IActionResult> CreateCar()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7273/api/Brands");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData1 = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData1);
+                List<SelectListItem> brandvalues = (from x in values
+                                                    select new SelectListItem
+                                                    {
+                                                        Text = x.name,
+                                                        Value = x.brandID.ToString()
+                                                    }).ToList();
+                ViewBag.BrandValues = brandvalues;
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCar(CreateCarDto createCarDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCarDto);
+            // veri tabanına veriyi gönderirken serialize yapmamızın sebebi, veriyi json formatına çevirmektir. Çünkü api controller'lar genellikle json formatında veri alır ve gönderirler.
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7273/api/Cars/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
